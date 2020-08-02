6988e4add5a7f38d676ae3c7f0b4dd9890abc646
a.evdokimov <a.evdokimov@net.local>
2020-08-03 03:59:44 +0700

    Добавлен sql-скрипт для добавления пользователей

---
+++ DocStorage/Sql/AddUsers.sql
@@ -0,0 +1,4 @@
+insert into UserDoc (FullName, Login, Password) values ('Иван Петров','i.petrov','Qwer1234');
+insert into UserDoc (FullName, Login, Password) values ('Сидр Мотрасов','s.motrasov','Qwer1234');
+insert into UserDoc (FullName, Login, Password) values ('Бэн Донг','b.dong','Qwer1234');
+insert into UserDoc (FullName, Login, Password) values ('Тест','test','t1');
